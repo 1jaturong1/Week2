217f48eb6668e507862b169cbded6a33b1646969
1jaturong1 <78637075+1jaturong1@users.noreply.github.com>
2021-03-28 14:55:35 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MIDEX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, x, y;
+            Console.Write("input number a : ");
+            Console.Write("input number b : ");
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            x = a;
+            y = b;
+            while (x != y)
+            {
+                if (x < y) 
+                { 
+                    x = x + a; 
+                }
+                else 
+                { 
+                    y = y + b; 
+                }
+                Console.Write(x);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
