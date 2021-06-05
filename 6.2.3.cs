a84828a3078032a203628ef04830b4f5b4c5286d
1jaturong1 <78637075+1jaturong1@users.noreply.github.com>
2021-06-05 22:37:41 +0700

    Add files via upload

---
+++ 6.2.3.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace การบ้าน6._2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Mealtime,hungrytime;
+            Console.Write("Input Mealtime");
+            Mealtime = double.Parse(Console.ReadLine());
+            hungrytime = double.Parse(Console.ReadLine());
+            if (Mealtime >= 0 && Mealtime <= 24)
+            {
+                hungrytime = Mealtime + 4;
+            }
+            if (hungrytime <= 6)
+            {
+                Console.WriteLine("hungrytime");
+            }
+            else
+            {
+                Console.WriteLine("Eat now!!!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
